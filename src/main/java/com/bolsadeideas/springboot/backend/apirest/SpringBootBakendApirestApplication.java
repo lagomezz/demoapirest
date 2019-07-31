00562b972ef2584a4fef71c1d827004c21ed839a
LuisArmandosalem <luisarmandogomezjimenez@gmail.com>
2019-07-31 15:52:20 -0500

    se hacen midificaciones al servicio y nombre de carpetas y archivos se crea el CRUD completo

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/SpringBootBakendApirestApplication.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/SpringBootBakendApirestApplication.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.spring.boot.bakend.apirest;
+package com.bolsadeideas.springboot.backend.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
